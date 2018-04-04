cd467ef9eddef158e54f46716141095943ecd8fb
rosexw <rose.xw.wan@gmail.com>
2018-04-03 20:01:27 -0400

    added hide notification feature

--- src/features/pokemon-list/pokemon-list.epics.js
+++ src/features/pokemon-list/pokemon-list.epics.js
@@ -1,9 +1,10 @@
 import 'rxjs/add/operator/mergeMap'; // important: make sure you import whichever rxjs operator you want to use
 import { Observable } from 'rxjs';
 
+import { SHOW_NOTIFICATION } from '../../components/notification/notification.types'
 import { FETCH_POKEMONS } from './pokemon-list.types';
 import { cleanPokemonRequest } from '../../utils/pokemonRequest';
-import { createShowNotificationAction, newMessageAction } from '../../components/notification/notification.action';
+import { createShowNotificationAction, createHideNotificationAction, newMessageAction } from '../../components/notification/notification.action';
 import { APP_LOAD } from '../app/app.types';
 import { fetchPokemons } from '../app/app.actions';
 
@@ -37,3 +38,9 @@ export const fetchPokemonsEpic = action$ =>
       level: 'success'
     })
   )
+
+  export const hideNotificationEpic = action$ =>
+    action$
+    .ofType(SHOW_NOTIFICATION)
+    .delay(2000)
+    .map(createHideNotificationAction)
